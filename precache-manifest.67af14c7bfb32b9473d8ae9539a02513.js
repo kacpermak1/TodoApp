f8c9a57a909d51f5f06feb8a20c456e690e5494b
Kacper <kacpermak24@gmail.com>
2020-02-28 03:35:57 +0000

    Updates

---
+++ precache-manifest.67af14c7bfb32b9473d8ae9539a02513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58ab12679724e24e3a5a1ad108aebcc",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "4240fd981debb6f57edf",
+    "url": "/TodoApp/static/css/main.d2e8d7d0.chunk.css"
+  },
+  {
+    "revision": "73fefba20bedc65412b3",
+    "url": "/TodoApp/static/js/2.50f3f2eb.chunk.js"
+  },
+  {
+    "revision": "0297e416e0ae6278314551fb47a206df",
+    "url": "/TodoApp/static/js/2.50f3f2eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4240fd981debb6f57edf",
+    "url": "/TodoApp/static/js/main.3c310bb8.chunk.js"
+  },
+  {
+    "revision": "531f6cf6dd5605bc8f43",
+    "url": "/TodoApp/static/js/runtime-main.bf8a0652.js"
+  },
+  {
+    "revision": "a2f358510ebfe32d0ed3dfae55aa64b6",
+    "url": "/TodoApp/static/media/1.a2f35851.jpg"
+  }
+]);
